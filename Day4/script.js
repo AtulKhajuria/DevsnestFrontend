729d57a7bcdf366e5832053a0e5e0e14cd399daf
AtulKhajuria <84517651+AtulKhajuria@users.noreply.github.com>
2021-06-22 20:36:07 +0530

    Add files via upload

---
+++ Day4/script.js
@@ -0,0 +1,13 @@
+setInterval(() => {
+    d = new Date();
+    htime = d.getHours();
+    mtime = d.getMinutes();
+    stime = d.getSeconds();
+    hrotation = 30*htime + mtime/2;
+    mrotation = 6*mtime ;
+    srotation = 6*stime;
+
+    hourhand.style.transform = `rotate(${hrotation}deg)`;
+    minhand.style.transform = `rotate(${mrotation}deg)`;
+    sechand.style.transform = `rotate(${srotation}deg)`;
+}, 1000);
