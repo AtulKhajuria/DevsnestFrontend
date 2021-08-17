996259b700bbb7f314a316a58666e5607be2eeac
AtulKhajuria <84517651+AtulKhajuria@users.noreply.github.com>
2021-08-17 19:07:53 +0530

    Add files via upload

---
+++ Day16/src/components/card.jsx
@@ -0,0 +1,11 @@
+const Card = () => {
+  return (
+    <div className="card">
+      <h2>React Card</h2>
+      <p>This is a Card</p>
+      <img src="https://starsunfolded.com/wp-content/uploads/2016/07/Roman-Reigns-36.jpg" />
+      
+    </div>
+  );
+};
+export default Card;
